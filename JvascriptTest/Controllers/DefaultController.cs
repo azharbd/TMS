1d78b71662b94839704bdced9bdc0bd6810c40d7
Azhar <ashraful.azhar@gmail.com>
2017-07-29 21:14:49 +0600

    initinal project

---
+++ JvascriptTest/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JvascriptTest.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult JTest()
+        {
+            return View();
+        }
+        public ActionResult ajax()
+        {
+            return View();
+        }
+        public JsonResult info()
+        {
+            
+
+            return Json(new { id = 1, name = "azhar" },JsonRequestBehavior.AllowGet);
+        }
+    }
+}
